ccc1d9ff6b6405f94058346bf5e720abcb45fe0a
cyrilmagayaga <cyrilmagayaga@gmail.com>
2024-02-08 22:55:28 +0800

    Update `main.go` and `goxenly.dll`

--- src/main.go
+++ src/main.go
@@ -1,36 +1,37 @@
 package main
 
 import "C"
+import "fmt"
 
 //export print_version
 func print_version() {
-    println("Xenly 0.1.0-preview3")
-    println("Copyright (c) 2023-2024 Cyril John Magayaga")
+    fmt.Println("Xenly 0.1.0-preview3")
+    fmt.Println("Copyright (c) 2023-2024 Cyril John Magayaga")
 }
 
 //export print_dumpversion
 func print_dumpversion() {
-    println("0.1.0-preview3")
+    fmt.Println("0.1.0-preview3")
 }
 
 //export print_help
 func print_help() {
-    println("Usage: xenly [input file]")
-    println("Options:")
-    println("  -h, --help                   Display this information.")
-    println("  -v, --version                Display compiler version information.")
-    println("  -dv, --dumpversion           Display the version of the compiler.")
-    println("  -dm, --dumpmachine           Display the compiler's target processor.")
-    println("  -os, --operatingsystem       Display the operating system.")
-    println("  -p, --path                   Display the path to the Xenly compiler executable.")
-    println("  --author                     Display the author information.")
-    println("\nFor bug reporting instructions, please see:")
-    println("<https://github.com/magayaga/xenly>")
+    fmt.Println("Usage: xenly [input file]")
+    fmt.Println("Options:")
+    fmt.Println("  -h, --help                   Display this information.")
+    fmt.Println("  -v, --version                Display compiler version information.")
+    fmt.Println("  -dv, --dumpversion           Display the version of the compiler.")
+    fmt.Println("  -dm, --dumpmachine           Display the compiler's target processor.")
+    fmt.Println("  -os, --operatingsystem       Display the operating system.")
+    fmt.Println("  -p, --path                   Display the path to the Xenly compiler executable.")
+    fmt.Println("  --author                     Display the author information.")
+    fmt.Println("\nFor bug reporting instructions, please see:")
+    fmt.Println("<https://github.com/magayaga/xenly>")
 }
 
 //export print_author
 func print_author() {
-	println("Cyril John Magayaga is the original author of Xenly programming language.")
+	fmt.Println("Cyril John Magayaga is the original author of Xenly programming language.")
 }
 
 func main() {}
