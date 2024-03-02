9d4d9115dcbe5f3ebf3d11b07d2669fd9eb27208
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-03-02 14:49:44 +0800

    Xenly's source code: Update `install.py`

--- install.py
+++ install.py
@@ -2,6 +2,8 @@
 # XENLY - high-level and general-purpose programming language
 # created, designed, and developed by Cyril John Magayaga (cjmagayaga957@gmail.com, cyrilmagayaga@proton.me).
 #
+# It is initially written in Python programming language.
+#
 import os
 
 # Compile xenly.c using gcc
