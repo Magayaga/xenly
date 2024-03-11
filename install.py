96ee3012bdae41eb9a5f47bb6a0c962a8d2fc862
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-03-11 15:04:02 +0800

    Xenly's source code: Update `install.py`
    GCC is the best compiler than Clang.

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 import os
 
-def compile_with_compiler(compiler):
-    compile_command = f"{compiler} src/xenly.c src/print_info.c src/color.c -o xenly -lm"
+def compile_with_gcc():
+    compile_command = "gcc src/xenly.c src/print_info.c src/color.c -o xenly -lm"
     compile_status = os.system(compile_command)
     
     # Check if compilation was successful
@@ -10,11 +10,4 @@ def compile_with_compiler(compiler):
     else:
         print("Compilation failed")
 
-# Choose compiler (GCC or Clang)
-compiler_choice = input("Choose compiler (GCC or Clang): ").lower()
-
-# Validate compiler choice
-if compiler_choice == "gcc" or compiler_choice == "clang":
-    compile_with_compiler(compiler_choice)
-else:
-    print("Invalid compiler choice. Please choose between GCC or Clang.")
+compile_with_gcc()
