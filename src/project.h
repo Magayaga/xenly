c34e6f2f33ba46ca36f7412da691059dc70b06ad
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-03-17 10:41:38 +0800

    Xenly's source code: Update `project.h`

--- src/project.h
+++ src/project.h
@@ -1,3 +1,10 @@
+/*
+ * XENLY - high-level and general-purpose programming language
+ * created, designed, and developed by Cyril John Magayaga (cjmagayaga957@gmail.com, cyrilmagayaga@proton.me).
+ *
+ * It is initially written in C programming language.
+ *
+ */
 #ifndef PROJECT_H
 #define PROJECT_H
 
