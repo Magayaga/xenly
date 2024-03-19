ea591bf589994e3670a118b78299f793f30e4b5b
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-03-19 20:06:44 +0800

    Xenly's source code: Update `install-gcc.py`

--- install-gcc.py
+++ install-gcc.py
@@ -1,7 +1,7 @@
 import os
 
 def compile_with_gcc():
-    compile_command = "gcc src/xenly.c src/print_info.c src/color.c -o xenly -lm"
+    compile_command = "gcc src/xenly.c src/print_info.c src/color.c src/error.c -o xenly -lm"
     compile_status = os.system(compile_command)
     
     # Check if compilation was successful
