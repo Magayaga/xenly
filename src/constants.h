0194091c115d7944630661ecc29004b3ce19cf1b
Cyril John Magayaga <cjmagayaga957@gmail.com>
2024-05-31 07:17:33 +0800

    Xenly's source code: Update and fixing `xenly.c` and adding `constants.h`

---
+++ src/constants.h
@@ -0,0 +1,27 @@
+/*
+ * XENLY - high-level and general-purpose programming language
+ * created, designed, and developed by Cyril John Magayaga (cjmagayaga957@gmail.com, cyrilmagayaga@proton.me).
+ *
+ * It is initially written in C programming language.
+ *
+ */
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Mathematical constants
+#define MATH_PI 3.14159265358979323846
+#define MATH_TAU 6.28318530717958647692
+#define MATH_E 2.71828182845904523536
+#define MATH_GOLDEN_RATIO 1.61803398874989484820
+#define MATH_SILVER_RATIO 2.41421356237309504880
+#define MATH_SUPERGOLDEN_RATIO 1.46557123187676802665
+
+// Physical constants
+#define PHYSICAL_SPEED_OF_LIGHT_MS 299792458
+#define PHYSICAL_SPEED_OF_LIGHT_KMH 1080000000
+#define PHYSICAL_SPEED_OF_LIGHT_MileS 186000
+
+#define PHYSICAL_GRAVTIATIONAL_CONSTANT_N_M2_KG2 0.0000000000667430
+
+
+#endif
--- src/xenly.c
+++ src/xenly.c
@@ -13,27 +13,13 @@
 #include <stdint.h>
 #include <ctype.h>
 #include <math.h>
+#include "constants.h"
 #include "color.h"
 #include "error.h"
 #include "xenly.h"
 #include "project.h"
 #include "print_info.h"
 // #include "goxenly.h"
-
-// Mathematical constants
-#define MATH_PI 3.14159265358979323846
-#define MATH_TAU 6.28318530717958647692
-#define MATH_E 2.71828182845904523536
-#define MATH_GOLDEN_RATIO 1.61803398874989484820
-#define MATH_SILVER_RATIO 2.41421356237309504880
-#define MATH_SUPERGOLDEN_RATIO 1.46557123187676802665
-
-// Physical constants
-#define PHYSICAL_SPEED_OF_LIGHT_MS 299792458
-#define PHYSICAL_SPEED_OF_LIGHT_KMH 1080000000
-#define PHYSICAL_SPEED_OF_LIGHT_MileS 186000
-
-#define PHYSICAL_GRAVTIATIONAL_CONSTANT_N_M2_KG2 0.0000000000667430
 
 // Evaluately condition
 bool evaluately_condition(const char* condition) {
