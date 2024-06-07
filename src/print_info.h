817e835674fdb55679f0f5069a61f15ea2bdd9ce
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-06-07 19:56:13 +0800

    Xenly's source code: Create `print_info.h`

---
+++ src/print_info.h
@@ -0,0 +1,22 @@
+/*
+ * XENLY - high-level and general-purpose programming language
+ * created, designed, and developed by Cyril John Magayaga (cjmagayaga957@gmail.com, cyrilmagayaga@proton.me).
+ *
+ * It is initially written in C programming language.
+ *
+ * It is available for Linux and Windows operating systems.
+ *
+ */
+#ifndef PRINTINFO_H
+#define PRINTINFO_H
+
+// Print version
+void print_version();
+
+// Print dump version
+void print_dumpversion();
+
+// Print author
+void print_author();
+
+#endif
