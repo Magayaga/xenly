761a6ff83410c7026b46135b460fcc6a3181ab93
Magayaga <cjmagayaga957@gmail.com>
2024-06-14 13:22:31 +0800

    Xenly's source code: Adding `make.bat` for Windows
    Adding batchfile for Windows

---
+++ make.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal
+
+rem Check for gcc
+where gcc >nul 2>&1
+if %ERRORLEVEL% EQU 0 (
+    set "compiler=gcc"
+) else (
+    rem Check for clang
+    where clang >nul 2>&1
+    if %ERRORLEVEL% EQU 0 (
+        set "compiler=clang"
+    ) else (
+        echo Error: No suitable C compiler found (gcc or clang)
+        exit /b 1
+    )
+)
+
+rem Compile xenly.c with the selected compiler
+%compiler% src\xenly.c src\color.c src\print_info.c -o xenly.exe -lm
+if %ERRORLEVEL% NEQ 0 (
+    echo Compilation failed.
+    exit /b 1
+)
+
+%compiler% src\xenly_math.c -shared -o math.dll -fPIC -lm
+if %ERRORLEVEL% NEQ 0 (
+    echo Compilation failed.
+    exit /b 1
+)
+
+rem Check if compilation was successful
+if exist xenly.exe (
+    echo Compilation successful. Running xenly programming language
+    xenly.exe
+) else (
+    echo Compilation failed.
+)
+
+endlocal
