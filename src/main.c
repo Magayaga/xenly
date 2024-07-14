7eb8df7cb59ad5a0194fbe0387308bc0a932ae37
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-07-14 11:12:55 +0800

    Xenly's source code: Adding `main.c`

---
+++ src/main.c
@@ -0,0 +1,33 @@
+/*
+ * XENLY - high-level and general-purpose programming language
+ * created, designed, and developed by Cyril John Magayaga (cjmagayaga957@gmail.com, cyrilmagayaga@proton.me).
+ *
+ * It is initially written in C programming language.
+ *
+ * It is available for Linux and Windows operating systems.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "xenly.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: xenly [input file]\n");
+        return 1;
+    }
+
+    FILE* file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Error: Could not open file %s\n", argv[1]);
+        return 1;
+    }
+
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file)) {
+        interpret_line(line);
+    }
+
+    fclose(file);
+    return 0;
+}
